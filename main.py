dbf759b1cb2b6a992efcae6d81a919c3b76de743
Roman Tarabrin <VacuuM2007@yandex.ru>
2023-04-17 19:15:04 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,47 @@
+class FlatIterator:
+
+    def __init__(self, list_of_list):
+        self.list_of_list = list_of_list
+
+    def __iter__(self):
+        self.counter = 0
+        self.i_counter = -1
+        return self
+
+    def __next__(self):
+        self.i_counter += 1
+        try:
+            return self.list_of_list[self.counter][self.i_counter]
+        except IndexError:
+            self.counter +=1
+            self.i_counter = 0
+            try:
+                return self.list_of_list[self.counter][self.i_counter]
+            except IndexError:
+                raise StopIteration
+
+
+list_of_lists_1 =[
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+
+def test_1():
+
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+
+
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+if __name__ == '__main__':
+    test_1()
